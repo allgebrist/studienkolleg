36e64194525d57af661da2a0207b54f857e97785
Allan A. Zea <allan.zea@ciens.ucv.ve>
2020-03-25 18:35:31 +0100

    added 2.22.dezimalzahl.pas

---
+++ 2.22.dezimalzahl.pas
@@ -0,0 +1,25 @@
+PROGRAM Dezimalzahl.pas;
+  VAR
+    dual : String[8];
+
+FUNCTION Konvertieren(x : String): Integer;
+  VAR
+    k : Integer;
+    temp : Integer;
+    dezi : Integer;
+  BEGIN
+    dezi := 0;
+    temp := 1;
+    FOR k := 8 DOWNTO 1 DO
+      BEGIN
+        dezi += (ord(x[k])-ord('0')) * temp;
+	temp *= 2;
+      END;
+    Konvertieren := dezi;
+  END;
+  
+
+BEGIN
+  READLN(dual);
+  WRITELN(Konvertieren(dual));
+END.
