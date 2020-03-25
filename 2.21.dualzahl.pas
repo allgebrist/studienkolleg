40644ae029c57d63ed139d738973ff047c304c1d
Allan A. Zea <allan.zea@ciens.ucv.ve>
2020-03-25 13:23:26 +0100

    added 2.21.dualzahl.pas

---
+++ 2.21.dualzahl.pas
@@ -0,0 +1,22 @@
+PROGRAM Dualzahl.pas;
+  VAR
+    n : Longint;
+    k : Integer;
+    stellen : Integer; (* Dualzahl *)
+    dual : array[0..10] of Integer;
+
+BEGIN
+  READ(n);
+  stellen := 0;
+  WHILE n > 0 DO
+    BEGIN
+      dual[stellen] := n MOD 2;
+      n := n DIV 2;
+      stellen += 1;
+    END;
+  
+  FOR k := stellen-1 DOWNTO 0 DO
+    WRITE(dual[k]);
+
+  WRITELN()
+END.
