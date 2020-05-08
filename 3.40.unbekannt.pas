892bcc0ba324b61c6ae0ffb37263737bbc1d4bac
Allan A. Zea <allan.zea98@gmail.com>
2020-05-08 12:24:14 +0200

    added 3.40.unbekannt.pas

---
+++ 3.40.unbekannt.pas
@@ -0,0 +1,30 @@
+PROGRAM Unbekannt;
+
+PROCEDURE Rule90(n : Integer);
+  VAR
+    x, y : Integer;
+    k : Integer;
+  BEGIN
+    y := n-1;
+    WHILE y >= 0 DO
+      BEGIN
+        FOR k := 0 TO y DO
+	  WRITE(' ');
+	
+	x := 0;
+	WHILE x + y < n DO
+	  BEGIN
+            IF x and y <> 0 THEN
+	      WRITE(' ', ' ')
+	    ELSE
+	      WRITE('* ');
+	    x += 1;
+	  END;
+	WRITELN();
+	y -= 1;
+      END;
+  END;
+
+BEGIN
+  Rule90(32);
+END.
